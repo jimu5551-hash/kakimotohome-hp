714f5f5ffcd6255e78a6c0ff91b78e23c4350f08
Kakimoto-Jimu <jimu5551@gmail.com>
2026-03-01 12:10:24 +0900

    Localize contact form (Japanese support)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,6 +18,7 @@ export const Contact = () => {
               <input type="hidden" name="_subject" value="【柿本ホーム産業】ウェブサイトからのお問い合わせ">
               <input type="hidden" name="_template" value="table">
               <input type="hidden" name="_captcha" value="false">
+              <input type="hidden" name="_language" value="ja">
 
               <div class="form-group">
                 <label for="inquiry-type">お問い合わせ内容</label>
